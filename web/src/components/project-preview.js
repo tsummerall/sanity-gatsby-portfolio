f0b2fc81b4d9aebc9cca25c62c719c836d2fc4dd
Thomas Summerall <thomassummerall@Thomass-MacBook-Pro.local>
2022-03-22 17:22:46 -0400

    Playing with gallery layout.

--- web/src/components/project-preview.js
+++ web/src/components/project-preview.js
@@ -14,8 +14,8 @@ function ProjectPreview(props) {
         {props.mainImage && props.mainImage.asset && (
           <img
             src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
-              .height(Math.floor((9 / 16) * 600))
+              .width(500)
+              .height(700)
               .url()}
             alt={props.mainImage.alt}
           />
